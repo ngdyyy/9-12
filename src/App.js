4bef2b35fd8aa6addda068bfd26192b7017be6e7
ngdyyy <nqduy130206@gmail.com>
2025-12-28 20:21:04 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function App() {
+  const name = 'Nguyen Quang Duy';
+  const job = 'Student';
+  const age = '19';
+  const favor = 'Football';
+  return (
+    <div className="App">
+      <h3>Name: {name}</h3>
+      <h3>Age: {age}</h3>
+      <h3>Job: {job}</h3>
+      <h3>Favor: {favor}</h3>
+      
+    </div>
+  );
+}
+
+export default App;
